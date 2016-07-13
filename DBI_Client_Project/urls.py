df6abc78533327cc40be209bd5f0d72bfa66fc23
ejarquin <ejarquin@thedataage.com>
2016-07-12 18:08:03 -0600

    agregue un DetailView y el index.html en la carpeta template para mostrar los datos iniciales al cargar la pagina

--- DBI_Client_Project/urls.py
+++ DBI_Client_Project/urls.py
@@ -19,5 +19,5 @@ from dbi_client_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', views.index, name="index"),
+    url(r'^project/(?P<pk>\d+)', views.home.as_view(), name="home"),
 ]
--- dbi_client_app/views.py
+++ dbi_client_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.generic import ListView, DetailView, TemplateView
 
 # Create your views here.
 from .models import *
@@ -8,4 +9,16 @@ from .process_project import ConfigProject
 def index(request):
 	project = DxinProyectos.objects.first()
 	newData = ConfigProject(project)
-	return HttpResponse(newData.getIndicadoresWithValue())
+	return JsonResponse(newData.getIndicadoresWithValue(), safe=False)
+
+class home(DetailView):
+	model = DxinProyectos
+	template_name = 'index.html'
+
+	def get_context_data(self, **kwargs):
+	    context = super(home, self).get_context_data(**kwargs)
+	    self.object = self.get_object()
+	    data = ConfigProject(self.object)
+	    context['data_table'] = data.getIndicadoresWithValue()
+	    context['indicadores_list'] = data.getIndicadoresToShow()
+	    return context
