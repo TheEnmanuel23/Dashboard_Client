1949f142863b08c13a43c2c00b8b0fc2f7a07f15
ejarquin <ejarquin@thedataage.com>
2016-09-27 10:55:52 -0600

    quite un console.log que no estaba ocupando e hice que el boton de APlicar filtros hiciera que al presionarlo se cerrar el modal agregandole la misma clase que tiene el boton de cerrar

--- dbi_client_app/static/js/backbone/views/app.js
+++ dbi_client_app/static/js/backbone/views/app.js
@@ -1,8 +1,5 @@
 dbClient.Views.app = Backbone.View.extend({
 	el: $('body'),
-	initialize: function(){
-		console.log("inicidado");
-	},
 	events: {
 		'click .indicador-visibles li a': 'indicador_click',
 		'click .dropdown-menu li a': 'select_filter'
